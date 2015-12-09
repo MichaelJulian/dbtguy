0ad0ab5a476a1cf1761d66c09ae1079a028c4be2
MichaelJulian <mikeyjmikeyj@gmail.com>
2015-12-09 14:24:52 -0800

    display author by email

--- db/migrate/20151209090011_devise_create_users.rb
+++ db/migrate/20151209090011_devise_create_users.rb
@@ -5,6 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.boolean :admin
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      #t.string :displayname,        null: false, default: "Anonymous"
 
       ## Recoverable
       t.string   :reset_password_token
