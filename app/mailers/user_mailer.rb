b6500810abcf528413a44cf4513818800477e996
MichaelJulian <mikeyjmikeyj@gmail.com>
2016-02-15 12:13:01 -0800

    edit mailer, finish https imgur

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,8 @@
 class UserMailer < ApplicationMailer
+  default from: "officialdbtguy@gmail.com"
+
+  def welcome_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Welcome to DbtGuy!')
+  end
 end
