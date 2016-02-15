b33109fffe5b5dc7b476debf0b421032018f6242
MichaelJulian <mikeyjmikeyj@gmail.com>
2016-02-15 13:48:58 -0800

    show author on all-tips, add user email action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ActiveRecord::Base
   has_many :tips
   acts_as_voter
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :send_welcome_email
+  def send_welcome_email
+    UserMailer.welcome_email(self).deliver_now
+  end
+
 end
